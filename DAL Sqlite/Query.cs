5be7a73960db08d522bac86518b6d7c5cb700ddb
Thomas V <thomasvalkenburg1@gmail.com>
2024-03-30 22:30:49 +0100

    add method to query only single item

---
+++ DAL Sqlite/Query.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+
+namespace DAL_Sqlite;
+
+public static class Query
+{
+    public static async Task<List<T>> Execute<T>(string query)
+    {
+        var     connection = DatabaseConnection.GetConnection();
+        List<T> tList;
+        
+        try
+        {
+            await connection.OpenAsync();
+            tList = connection.Query<T>(query).ToList();
+            await connection.CloseAsync();
+        }
+        // ReSharper disable once RedundantCatchClause
+        catch (Exception)
+        {
+            throw;
+        }
+
+        return tList;
+    }
+
+    public static async Task<T> ExecuteFirst<T>(string query)
+    {
+        var connection = DatabaseConnection.GetConnection();
+        T obj;
+        
+        try
+        {
+            await connection.OpenAsync();
+            obj = connection.QuerySingle<T>(query);
+            await connection.CloseAsync();
+        }
+        // ReSharper disable once RedundantCatchClause
+        catch (Exception)
+        {
+            throw;
+        }
+
+        return obj;
+    }
+}
