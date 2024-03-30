58edebd7ef5029a11a1041869661410f81a892a2
Thomas V <thomasvalkenburg1@gmail.com>
2024-03-30 22:27:00 +0100

    Create gateways for inventory and customer

---
+++ DAL Sqlite/Gateways/InventoryGateway.cs
@@ -0,0 +1,11 @@
+﻿namespace DAL_Sqlite.Gateways;
+
+public class InventoryGateway
+{
+    public async Task<List<Domain.Models.Inventory>> GetAllInventories(int customerId)
+    {
+        var inventoryList = await Query.Execute<Data_Access_Models.Inventory>($"SELECT * FROM Inventory WHERE CustomerId={customerId}");
+
+        return Data_Access_Models.Inventory.ConvertToDomainClass(inventoryList);
+    }
+}
