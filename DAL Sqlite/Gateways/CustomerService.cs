961676af39d082b01fd8ebc07a22d8b07cce7f95
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2024-04-04 11:11:25 +0200

    rename gateway to service and implement interfaces

---
+++ DAL Sqlite/Gateways/CustomerService.cs
@@ -0,0 +1,11 @@
+﻿using Interfaces;
+
+namespace DAL_Sqlite.Gateways;
+
+public class CustomerService : ICustomer
+{
+    public Domain.Models.Customer GetCustomer(int customerId)
+    {
+        return Query.ExecuteFirst<Domain.Models.Customer>($"SELECT * FROM Customer WHERE Id={customerId}").GetAwaiter().GetResult();
+    }
+}
---
+++ DAL Sqlite/Gateways/InventoryService.cs
@@ -0,0 +1,11 @@
+﻿namespace DAL_Sqlite.Gateways;
+
+public class InventoryService : Interfaces.IInventory
+{
+    public List<Domain.Models.Inventory> GetAllInventories(int customerId)
+    {
+        var inventoryList = Query.Execute<Data_Access_Models.Inventory>($"SELECT * FROM Inventory WHERE CustomerId={customerId}").GetAwaiter().GetResult();
+
+        return Data_Access_Models.Inventory.ConvertToDomainClass(inventoryList);
+    }
+}
