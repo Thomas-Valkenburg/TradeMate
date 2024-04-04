fcaf8d4ac5f32084ee2f876de3f49c39a279e3af
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2024-04-04 11:11:53 +0200

    remove interfaces and get interfaces instead

---
+++ DAL Factory/Factory.cs
@@ -0,0 +1,11 @@
+﻿using Interfaces;
+
+namespace DAL_Factory;
+
+public class Factory
+{
+    public static IInventory GetInventoryGateway()
+    {
+        return new DAL_Sqlite.Gateways.InventoryService();
+    }
+}
