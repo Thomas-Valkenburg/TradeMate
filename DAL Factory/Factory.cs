c7dbcb550fd1fb958a4aef26b917065ac3bcc2af
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2024-04-08 13:03:34 +0200

    add selection for different dal interface implementations in factory

--- DAL Factory/Factory.cs
+++ DAL Factory/Factory.cs
@@ -1,11 +1,27 @@
-﻿using Interfaces;
+﻿using DAL_EF_Core.Services;
+using DAL_Sqlite.Services;
+using DAL_Test.Services;
+using Interfaces;
 
 namespace DAL_Factory;
 
-public class Factory
+public static class Factory
 {
-    public static IInventory GetInventoryGateway()
+    public static IDal GetService(ServiceType type)
     {
-        return new DAL_Sqlite.Gateways.InventoryService();
+        return type switch
+        {
+            ServiceType.Sqlite => new SqLiteService(),
+            ServiceType.EfCore => new EfCoreService(),
+            ServiceType.Test => new TestService(),
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null),
+        };
+    }
+
+    public enum ServiceType
+    {
+        Sqlite,
+        EfCore,
+        Test
     }
 }
