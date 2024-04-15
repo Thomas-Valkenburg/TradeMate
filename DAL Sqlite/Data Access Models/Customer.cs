339bf438233a44073224db222853d2f36116b84a
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2024-04-15 15:02:41 +0200

    add model and functions for customer database model

--- DAL Sqlite/Data Access Models/Customer.cs
+++ DAL Sqlite/Data Access Models/Customer.cs
@@ -1,5 +1,41 @@
-﻿namespace DAL_Sqlite.Data_Access_Models;
+﻿using DAL_Sqlite.Services;
+using Dapper.Contrib.Extensions;
 
-internal class Customer
+namespace DAL_Sqlite.Data_Access_Models;
+
+[Table("Customer")]
+internal class Customer : DbAccessModel
 {
+    [Key]
+    public required int Id { get; init; }
+
+    public required string Name { get; init; }
+
+    public required string Email { get; init; }
+
+    internal Domain.Models.Customer ConvertToDomain()
+    {
+        return new Domain.Models.Customer
+        {
+            Id       = Id,
+            Name     = Name,
+            Email    = Email,
+            Inventory = new SqLiteService().GetAllInventories(Id)
+        };
+    }
+
+    internal static Customer ConvertToDataAccess(Domain.Models.Customer customer)
+    {
+        return new Customer
+        {
+            Id    = customer.Id,
+            Name  = customer.Name,
+            Email = customer.Email
+        };
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
 }
