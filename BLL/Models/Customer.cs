5e1a6ce7cd0dc99baeed4c5e751187617a3d2c1e
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2024-04-15 14:53:57 +0200

    add save customer function

--- BLL/Models/Customer.cs
+++ BLL/Models/Customer.cs
@@ -7,4 +7,8 @@ public class Customer(Factory.ServiceType serviceType) : Domain.Models.Customer
 {
     private readonly IDal _service = Factory.GetService(serviceType);
     
+    public Result SaveCustomer()
+    {
+        return _service.CreateCustomer(this);
+    }
 }
