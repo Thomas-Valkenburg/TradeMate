fba2cb8f483c4df7f0601a4d78f90746f6d83bd7
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2024-04-19 14:42:19 +0200

    add service

--- BLL/Models/StockItem.cs
+++ BLL/Models/StockItem.cs
@@ -1,6 +1,19 @@
-﻿namespace BLL.Models;
+﻿using DAL_Factory;
+using Interfaces;
+
+namespace BLL.Models;
 
 public class StockItem : Domain.Models.StockItem
 {
+    private readonly IDal _service;
     
+    public StockItem(Factory.ServiceType serviceType)
+    {
+        _service = Factory.GetService(serviceType);
+    }
+
+    public StockItem(IDal service)
+    {
+        _service = service;
+    }
 }
