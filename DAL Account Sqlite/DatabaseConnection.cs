85a8f6caa0bfb5b136954e04b49e3f5f845d633f
Thomas V <thomasvalkenburg1@gmail.com>
2024-04-21 22:10:51 +0200

    Add Account Sqlite Project

---
+++ DAL Account Sqlite/DatabaseConnection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.Configuration;
+
+namespace DAL_Account_Sqlite;
+
+public static class DatabaseConnection
+{
+    private static SqliteConnection? _connection;
+
+    private static SqliteConnection CreateConnection()
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        return new SqliteConnection(configuration.GetConnectionString("AccountConnection")?.Replace("~", AppContext.BaseDirectory));
+    }
+
+    public static SqliteConnection GetConnection()
+    {
+        return _connection ??= CreateConnection();
+    }
+}
