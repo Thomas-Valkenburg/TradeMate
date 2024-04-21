739664c9153ee2e0831ca7062247303fa340dad6
Thomas V <thomasvalkenburg1@gmail.com>
2024-04-21 23:50:31 +0200

    Add actions for login and singup

--- Web/Controllers/LoginController.cs
+++ Web/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers;
 
@@ -7,5 +8,30 @@ public class LoginController : BaseController
     public ActionResult Index()
     {
         return View();
+    }
+
+    [HttpPost]
+    public ActionResult LoginPost(string username, string password)
+    {
+	    var result = Account.TryLogin(username, password);
+
+	    if (!result.Success) ViewData["LoginError"] = result.ErrorMessage;
+        
+        
+
+		return RedirectToAction("Index");
+    }
+
+    public ActionResult Signup()
+    {
+	    return View();
+    }
+
+    [HttpPost]
+    public ActionResult SignupPost(string username, string email, string password)
+    {
+	    Account.CreateAccount(username, password, email);
+
+	    return RedirectToAction("Index");
     }
 }
