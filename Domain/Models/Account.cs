5eaee451c4321a16913748ea983e052a922ffe53
Thomas V <thomasvalkenburg1@gmail.com>
2024-04-21 23:47:49 +0200

    change unchangeable properties to init only and add dapper attributes

--- Domain/Models/Account.cs
+++ Domain/Models/Account.cs
@@ -1,16 +1,21 @@
-﻿namespace Domain.Models;
+﻿using Dapper.Contrib.Extensions;
+
+namespace Domain.Models;
 
 public class Account
 {
-	public int Id { get; set; }
-	
+	[Computed]
+	public int Id { get; init; }
+
+	[ExplicitKey]
 	public required string Username { get; set; }
-	
+
+	[ExplicitKey]
 	public required string Password { get; set; }
 	
 	public required string Email { get; set; }
 	
-	public required DateTime CreationDate { get; set; }
+	public required DateTime CreationDate { get; init; }
 	
-	public required int CustomerId { get; set; }
+	public required int CustomerId { get; init; }
 }
