db571d23839f424b8321f2771ea93ccd1af8029d
Thomas V <thomasvalkenburg1@gmail.com>
2024-05-21 22:53:49 +0200

    add AddInventory actions

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace Web.Controllers;
 
 public class HomeController : BaseController
 {
+    [HttpGet]
     public ActionResult Index(int? inventory = null)
     {
         var customer = Customer.TryGetCustomer(int.Parse(HttpContext.Session.GetString("CustomerId")!), Program.ServiceType).Value!;
@@ -14,5 +15,21 @@ public class HomeController : BaseController
         ViewData["Inventory"] = inventory;
 
         return View(new InventoriesViewModel(customer.GetInventories()));
+    }
+
+    [HttpGet]
+    public ActionResult AddInventory()
+    {
+	    return View();
+    }
+
+    [HttpPost]
+    public ActionResult AddInventory(string name)
+    {
+	    var customer = Customer.TryGetCustomer(int.Parse(HttpContext.Session.GetString("CustomerId")!), Program.ServiceType).Value;
+
+	    var result = customer?.AddInventory(name);
+
+	    return RedirectToAction("Index", "Home", routeValues: $"result={result?.ErrorMessage}");
     }
 }
