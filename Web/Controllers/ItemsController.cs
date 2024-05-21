43c13d80b851138e8027e41e2859d2d4425d81dd
Thomas V <thomasvalkenburg1@gmail.com>
2024-05-21 22:54:09 +0200

    add AddItem actions

--- Web/Controllers/ItemsController.cs
+++ Web/Controllers/ItemsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Globalization;
+using BLL.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers;
 
@@ -7,6 +9,27 @@ public class ItemsController : BaseController
 	public ActionResult Index(int itemId)
 	{
 		return View();
+	}
+
+	public ActionResult Add(int inventoryId)
+	{
+		var inventory = Inventory.TryGetInventory(inventoryId, Program.ServiceType);
+
+		if (!inventory.Success || inventory.Value is null) return Error();
+
+		(string name, int id) data = (inventory.Value.Name, inventory.Value.Id);
+
+		return View(data);
+	}
+
+	[HttpPost]
+	public ActionResult Add(int inventoryId, string name, string barcode, int amount, string price)
+	{
+		var inventory = Inventory.TryGetInventory(inventoryId, Program.ServiceType).Value;
+
+		inventory?.AddStockItem(name, barcode, amount, decimal.Parse(price, CultureInfo.InvariantCulture));
+
+		return RedirectToAction("Index", "Home", new { inventory = inventoryId });
 	}
 
 	[HttpPost]
