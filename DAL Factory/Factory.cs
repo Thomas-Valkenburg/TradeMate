90a36132ae5e7340ba6117e03578c3c596b42b7b
Thomas Valkenburg <thomasvalkenburg1@gmail.com>
2024-05-23 14:44:29 +0200

    Change factory to use existing services over new ones

--- DAL Factory/Factory.cs
+++ DAL Factory/Factory.cs
@@ -8,20 +8,45 @@ namespace DAL_Factory;
 
 public static class Factory
 {
+	private static readonly List<IAccountDataAccessLayer> AccountServices = [];
+	private static readonly List<IDataAccessLayer>        Services        = [];
+
 	public static IAccountDataAccessLayer GetAccountService()
 	{
-		return new AccountSqLiteService();
+		var service = AccountServices.Find(service => service.GetType() == typeof(AccountSqLiteService)) as AccountSqLiteService;
+
+        if (service is not null) return service;
+
+        var newService = new AccountSqLiteService();
+
+        AccountServices.Add(new AccountSqLiteService());
+
+        return newService;
 	}
     
     public static IDataAccessLayer GetDataService(ServiceType type)
     {
-        return type switch
+	    IDataAccessLayer? service = type switch
+	    {
+		    ServiceType.Sqlite => Services.Find(service => service.GetType() == typeof(SqLiteService)) as SqLiteService,
+		    ServiceType.EfCore => Services.Find(service => service.GetType() == typeof(EfCoreService)) as EfCoreService,
+		    ServiceType.Test   => Services.Find(service => service.GetType() == typeof(TestService)) as TestService,
+		    _                  => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+	    };
+
+	    if (service is not null) return service;
+
+	    service = type switch
         {
             ServiceType.Sqlite => new SqLiteService(),
             ServiceType.EfCore => new EfCoreService(),
             ServiceType.Test => new TestService(),
             _ => throw new ArgumentOutOfRangeException(nameof(type), type, null),
         };
+
+        Services.Add(service);
+
+        return service;
     }
 
     public enum ServiceType
